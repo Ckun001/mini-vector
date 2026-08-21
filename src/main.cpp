50b2396f143baf614d0e39cdb3b2a28fb96d0c2a
Ckun001 <2035255838@qq.com>
2026-08-21 16:05:33 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "Mini Vector DB started successfully!" << std::endl;
     std::cout << "Build time: " << __DATE__ << " " << __TIME__ << std::endl;
     LOG_INFO("Mini Vector DB started successfully!");
-    LOG_INFO("Build time: %s %s", __DATE__,__TIME__);
+    LOG_INFO("Build time: %s %s", __DATE__, __TIME__);
 
 
     
