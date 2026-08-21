d31c643dafdce3a6f2e6f136fe39fa4c449be9b7
Ckun001 <2035255838@qq.com>
2026-08-21 11:01:16 +0800

    Add main function to display startup message

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "🚀 Mini Vector DB 启动成功！" << std::endl;
+    std::cout << "当前时间: " << __DATE__ << " " << __TIME__ << std::endl;
+    return 0;
+}
