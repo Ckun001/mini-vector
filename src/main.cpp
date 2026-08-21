2a9a2d4d14960fa02dc9417481bc836139e04060
Ckun001 <2035255838@qq.com>
2026-08-21 15:36:31 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include"util/log.h"//引入日志模块
 
 int main() {
     std::cout << "Mini Vector DB started successfully!" << std::endl;
     std::cout << "Build time: " << __DATE__ << " " << __TIME__ << std::endl;
+    LOG_INFO("Mini Vector DB started successfully!");
+    LOG_INFO("Build time: " << __DATE__ << " " << __TIME__);
+
+
+    
     return 0;
 }
